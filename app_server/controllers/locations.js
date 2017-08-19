b044e15b7243c40df6a2fc616373dad9c037577f
ckma23 <curtiskma@gmail.com>
2017-08-18 20:55:27 -0700

    Finished with Static Pages

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -5,10 +5,10 @@ module.exports.homelist = function (req,res){
 
 // get the page
 module.exports.locationInfo = function (req,res){
-  res.render('index',{title:"Location Info"});
+  res.render('location-info',{title:"Location Info"});
 };
 
 // get the page
 module.exports.addReview = function (req,res){
-  res.render('index',{title:"Add Review"})
+  res.render('location-review-form',{title:"Add Review"})
 }
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,5 +1,5 @@
 // this is a controller named about that once the route makes a request the about controller will render the "view"
-// Get the about page 
+// Get the about page
 module.exports.about = function(req,res){
-  res.render('index', {title:"About"});
+  res.render('generic-text', {title:"About"});
 }
